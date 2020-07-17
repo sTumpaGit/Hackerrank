0a94e306a56f691a4295fc671a297c3b0850c9b0
sTumpaGit <68402283+sTumpaGit@users.noreply.github.com>
2020-07-17 18:05:14 +0530

    Update Apple and Orange.java

--- Practice/Algorithms/Implementation/Apple and Orange.java
+++ Practice/Algorithms/Implementation/Apple and Orange.java
@@ -1,59 +1,32 @@
-import java.util.Scanner;
-import java.io.*;
+import java.util.*;
 
-public class AppleAndOrange{
+public class Solution{
     public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int s = scan.nextInt();
+        int t = scan.nextInt();
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        
+        int[] apple = new int[m];
+        for(int i=0; i<m; i++) 
+            apple[i]=scan.nextInt();
 
-            Scanner input = new Scanner(System.in);
-            
-            int countApple = 0;
-            int countOrange =0;
-            int st[] = new int[2];
-            //System.out.println("enter starting and ending point of Sam's house respectively- ");
-            for(int i=0; i<2; i++){ 
-                st[i] = input.nextInt();
-            }
-
-            int[] ab = new int[2];
-            //System.out.println("enter the points of apple tree and orrange tree respectively - ");
-            for(int i=0; i<2; i++){ 
-                ab[i] = input.nextInt();
-            }
-
-            int[] mn = new int[2];
-            //System.out.println("enter the number of apples and orranges fallen from trees respectively - ");
-            for(int i=0; i<2; i++){ 
-                mn[i] = input.nextInt();
-            }
-
-            int x= mn[0];
-            int y = mn[1];
-
-            int apples[] = new int[x];
-            int oranges[] = new int[y];
-
-            for(int i=0; i<x; i++){
-                apples[i] = input.nextInt();
-            }
-
-            for(int i=0; i<y; i++){
-                oranges[i] = input.nextInt();
-            }
-            //For Aplle Tree
-            for(int i=0; i<x; i++){
-                if(((ab[0]+apples[i]) >= st[0]) && ((ab[0]+apples[i]) <= st[1])){
-                    countApple += 1;
-                }
-            }
-
-            //For Orange Tree
-            for(int i=0; i<y; i++){
-                if(((ab[1]+oranges[i]) <= st[1]) && ((ab[1]+oranges[i]) >= st[0])){
-                    countOrange += 1;
-                }
-            }
-
-            System.out.println(countApple);
-            System.out.println(countOrange);
+        int[] orange = new int[n];
+        for(int i=0; i<n; i++)
+            orange[i]=scan.nextInt();
+        System.out.println(result(s,t,a,apple));   // Apple tree
+        System.out.println(result(s,t,b,orange));   // Orange tree
     }
+
+    public static int result(int start , int end , int tree , int[] fruits){
+        int count = 0;
+        for(int i=0; i<fruits.length; i++){
+            if((fruits[i]+tree)>=start && (fruits[i]+tree)<=end) count++;
+        }
+        return count;
+    }
+
 }
